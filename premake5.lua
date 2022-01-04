9cdba107b4ca9d0b26c6fcda59f0323685d15ea9
Antischoker16 <oberhauser.antonia@student.htlwrn.ac.at>
2022-01-04 17:38:31 +0100

    added premake

---
+++ premake5.lua
@@ -0,0 +1,72 @@
+-- Retrieve the project name
+newoption { trigger = "projectname", description = "Name of the generated project" }
+local projectName = _OPTIONS["projectname"]
+if projectName == nil then print("The project name was not specified! --projectname=YourApplication") end
+
+-- Main Solution
+workspace (projectName)
+    configurations { "Debug", "Release" }
+
+    --platforms { "x86", "x64" }
+    --defaultplatform "x86"
+    startproject (projectName)
+    
+    filter "system:not windows"
+    	location "build"
+    filter {}
+
+-- Include the subprojects
+include "modules/FLAME_Protocol"
+
+-- Actual project
+project (projectName)
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+    location "build"
+    targetdir "bin/%{cfg.buildcfg}"
+    targetname (projectName)
+
+    pchheader "pch.h"
+    pchsource "src/pch.cpp"
+
+    -- Configuration filters, filters are active up to the next filter statement
+    -- Indentation is purely visual
+
+    filter "configurations:Debug"
+        defines { "DEBUG", "_DEBUG", "NDEPLOY" }
+        runtime "Debug"
+        symbols "On"
+        optimize "Off"
+
+    filter "configurations:Release"
+        defines { "NDEBUG", "NDEPLOY" }
+        runtime "Release"
+        symbols "On"
+        optimize "On"
+
+    filter "configurations:Deploy"
+        defines { "NDEBUG", "DEPLOY" }
+        runtime "Release"
+        symbols "Off"
+        optimize "On"
+
+    filter {}
+
+
+    -- Include directories
+    local _includedirs = { 
+        _SCRIPT_DIR .. "/include"
+    }
+    includedirs (_includedirs)
+
+    
+    -- Main source files
+    files { "include/**", "src/**" }
+
+    -- NetLib dependency
+    dependson "FLAME_Protocol"
+    includedirs (FLAMEPROTOCOL_INCLUDE_DIRS)
+    libdirs (FLAMEPROTOCOL_LINK_DIRS)
+    links (FLAMEPROTOCOL_LINKS)
