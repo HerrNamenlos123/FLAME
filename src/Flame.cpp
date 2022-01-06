31ff9d6dcc43822d12effc3ea5db1983a8408992
Antischoker16 <oberhauser.antonia@student.htlwrn.ac.at>
2022-01-06 18:22:52 +0100

    Automated commit

--- src/Flame.cpp
+++ src/Flame.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <iostream>
 #include <chrono>
+#include <sstream>
 
 #include "Log.h"
 #include "Flame.h"
@@ -10,26 +11,45 @@
 
 FLAME::Instance flameInstance;
 
+uint32_t ipAdress = 0;
+
+std::string ipToStr2(uint32_t ip)
+{
+	std::stringstream ipStr;
+	ipStr << (ip & 0xff) << ".";
+	ipStr << ((ip >> 8) & 0xff) << ".";
+	ipStr << ((ip >> 16) & 0xff) << ".";
+	ipStr << ((ip >> 24) & 0xff);
+	return ipStr.str();
+}
 uint64_t getMicros() {
 	using namespace std::chrono;
 	return duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
 }
 
 void packetReceived(uint8_t* packet, size_t packetSize) {
-	//printf("Recived packet%s", packet);
+	
+	if (packetSize == 0) return;
+	if (packetSize == FLAME_PROTOCOL_DISCOVERY_RESPONSE_LENGTH) {
 
-	FLAME_Protocol::Packet pk;
-	if (packetSize == FLAME_PROTOCOL_STANDARD_PACKET_LENGTH) {
-		memcpy(pk.data, packet, packetSize);
-		FLAME_Protocol::PacketData pd1;
+		FLAME_Protocol::DiscoveryResponse dr;
+		if (FLAME_Protocol::parsePacket(&dr, packet)) {
+			ipAdress = dr.ipAddress;
+			LOG_WARN("IP = {}", dr.ipAddress);
+		}
+		else {
+			LOG_ERROR("Packet invalid");
+		}
 
-		if (parsePacket(&pk, &pd1)) {
-			LOG_WARN("Axis1 = {}", pd1.axis1);
-			LOG_WARN("Axis2 = {}", pd1.axis2);
-			LOG_WARN("Axis3 = {}", pd1.axis3);
-			LOG_WARN("Axis4 = {}", pd1.axis4);
-			LOG_WARN("id = {}", pd1.id);
-			LOG_WARN("Additional = {}", pd1.additional);
+	}
+
+	if (packetSize == FLAME_PROTOCOL_REVIEW_PACKET_LENGTH) {
+
+		FLAME_Protocol::ReviewPacket rp;
+
+		if (parsePacket(&rp, packet)) {
+			LOG_WARN("id = {}", rp.id);
+			LOG_WARN("Data = {}", rp.data);
 
 		}
 		else {
@@ -44,58 +64,50 @@ void FlameTest() {
 
 	auto interfaces = NetLib::GetNetworkInterfaces();
 
+	//Send Discovery Packet
+	uint8_t discoveryBuffer[1];
+	FLAME_Protocol::generatePacket(discoveryBuffer);
+
 	for (auto& ifc : interfaces) {
 		LOG_WARN("IP: {}, Broadcast address: {}", ifc.address, NetLib::CreateBroadcastAddress(ifc));
+
+		NetLib::UDPClient uc(NetLib::CreateBroadcastAddress(ifc), 22500);
+		uc.send(discoveryBuffer, 1);
 	}
 
 	return;
 
 	NetLib::SetLogLevel(NetLib::LOG_LEVEL_INFO);
 
-	FLAME_Protocol::PacketData pd;
-	pd.axis1 = 10;
-	pd.axis2 = 20;
-	pd.axis3 = 30;
-	pd.axis4 = 40;
-	pd.id = 1;
-	pd.additional = 22;
+	//Create Controll Packet
+	FLAME_Protocol::ControlPacket cp;
+	cp.axis1 = 10;
+	cp.axis2 = 20;
+	cp.axis3 = 30;
+	cp.axis4 = 40;
+	cp.id = 1;
+	cp.additional = 22;
 
-	FLAME_Protocol::Packet packet;
-	generatePacket(&packet, &pd);
+	uint8_t buffer[23];
 
-	
-	FLAME_Protocol::PacketData pd1;
-	if (FLAME_Protocol::parsePacket(&packet, &pd1)) {
-		if (pd.axis1 == pd1.axis1) {
-			std::cout << "Axis1 are the same" << std::endl;
-		}
-		if (pd.axis2 == pd1.axis2) {
-			std::cout << "Axis2 are the same" << std::endl;
-		}
-		if (pd.axis3 == pd1.axis3) {
-			std::cout << "Axis3 are the same" << std::endl;
-		}
-		if (pd.axis4 == pd1.axis4) {
-			std::cout << "Axis4 are the same" << std::endl;
-		}
-		if (pd.id == pd1.id) {
-			std::cout << "ID are the same" << std::endl;
-		}
-		if (pd.additional == pd1.additional) {
-			std::cout << "Additionals are the same" << std::endl;
-		}
+	generatePacket( &cp, buffer );
+
+
+	if (FLAME_Protocol::parsePacket(&cp, buffer)) {
+		LOG_WARN("Axis1 = {}", cp.axis1);
+		LOG_WARN("Axis2 = {}", cp.axis2);
+		LOG_WARN("Axis3 = {}", cp.axis3);
+		LOG_WARN("Axis4 = {}", cp.axis4);
+		LOG_WARN("id = {}", cp.id);
+		LOG_WARN("Additional = {}", cp.additional);
 
 	}
 	else {
 		std::cout << "Wrong Packet" << std::endl;
 	}
 
-	NetLib::UDPClient uc("10.20.85.175", 22500);
-	//NetLib::UDPClient uc("10.20.255.255", 22500);
+	NetLib::UDPClient uc(ipToStr2(ipAdress), 22500);
 	
-	
-	
-
 	NetLib::UDPServer us(packetReceived, 22500);
 
 	uint64_t old = getMicros();
@@ -104,11 +116,11 @@ void FlameTest() {
 		if (getMicros() >= old + interval) {
 			old = getMicros();
 
-			uc.send(packet.data, 23);
-			uc.send(packet.data, 23);
-			uc.send(packet.data, 23);
-			uc.send(packet.data, 23);
-			uc.send(packet.data, 23);
+			uc.send(buffer, 23);
+			uc.send(buffer, 23);
+			uc.send(buffer, 23);
+			uc.send(buffer, 23);
+			uc.send(buffer, 23);
 		}
 	}
 
